651987b27b1316413f7c0a3388e25ca40fb6bceb
asj3733 <asj3733@yahoo.com.tw>
2026-07-11 17:43:37 +0800

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,55 @@
+const menu = {
+
+  "麵類":[
+    {name:"肉燥乾麵（小）",price:55},
+    {name:"肉燥乾麵（大）",price:65},
+    {name:"肉燥米粉（小）",price:55},
+    {name:"肉燥米粉（大）",price:65},
+    {name:"湯麵（小）",price:55},
+    {name:"湯麵（大）",price:65},
+    {name:"麻醬麵（小）",price:60},
+    {name:"麻醬麵（大）",price:70},
+    {name:"鮮蝦餛飩乾麵（小）",price:70},
+    {name:"鮮蝦餛飩乾麵（大）",price:80},
+    {name:"鮮蝦餛飩湯麵（小）",price:70},
+    {name:"鮮蝦餛飩湯麵（大）",price:80}
+  ],
+
+  "關東煮":[
+    {name:"黑輪",price:10},
+    {name:"米血",price:10},
+    {name:"魚板",price:10},
+    {name:"蛋丸",price:10},
+    {name:"菜頭",price:15},
+    {name:"竹輪",price:15},
+    {name:"貢丸",price:20},
+    {name:"福州丸",price:20},
+    {name:"黃金魚板",price:20},
+    {name:"高麗菜捲",price:20},
+    {name:"苦瓜封",price:40}
+  ],
+
+  "小菜":[
+    {name:"麵輪",price:10},
+    {name:"滷蛋",price:15},
+    {name:"荷包蛋",price:15},
+    {name:"蔥蛋",price:40},
+    {name:"香腸",price:25},
+    {name:"手工大腸",price:75}
+  ],
+
+  "湯類":[
+    {name:"菜頭湯",price:35},
+    {name:"魚丸湯",price:35},
+    {name:"貢丸湯",price:40},
+    {name:"鮮蝦餛飩湯",price:45}
+  ],
+
+  "其他":[
+    {name:"肉燥飯",price:35},
+    {name:"白飯",price:15},
+    {name:"燙青菜",price:40},
+    {name:"水餃",price:50}
+  ]
+
+};
