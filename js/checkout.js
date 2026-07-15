001c928fcb863dfb7796e1113f9bf72b467a4015
asj3733 <asj3733@yahoo.com.tw>
2026-07-15 20:26:46 +0800

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -1,3 +1,23 @@
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-console.log(cart);
+const orderList = document.getElementById("order-list");
+
+let html = "";
+let total = 0;
+
+cart.forEach(item => {
+
+    html += `
+        <p>
+            ${item.name} × ${item.qty}
+           　NT$${item.price * item.qty}
+        </p>
+    `;
+
+    total += item.price * item.qty;
+
+});
+
+html += `<hr><h3>合計 NT$${total}</h3>`;
+
+orderList.innerHTML = html;
