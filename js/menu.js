9e19bb16f8bb00b9fb479146f1833bb11c9345e9
asj3733 <asj3733@yahoo.com.tw>
2026-07-20 01:35:53 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,32 +1,32 @@
 const menu = {
 
   "麵類":[
-    {name:"肉燥乾麵（小）",price:55},
-    {name:"肉燥乾麵（大）",price:65},
-    {name:"肉燥米粉（小）",price:55},
-    {name:"肉燥米粉（大）",price:65},
-    {name:"湯麵（小）",price:55},
-    {name:"湯麵（大）",price:65},
-    {name:"麻醬麵（小）",price:60},
-    {name:"麻醬麵（大）",price:70},
-    {name:"鮮蝦餛飩乾麵（小）",price:70},
-    {name:"鮮蝦餛飩乾麵（大）",price:80},
-    {name:"鮮蝦餛飩湯麵（小）",price:70},
-    {name:"鮮蝦餛飩湯麵（大）",price:80}
+    {name:"肉燥乾麵（小）",price:55,type:"noodle"},
+    {name:"肉燥乾麵（大）",price:65,type:"noodle"},
+    {name:"肉燥米粉（小）",price:55,type:"noodle"},
+    {name:"肉燥米粉（大）",price:65,type:"noodle"},
+    {name:"湯麵（小）",price:55,type:"noodle"},
+    {name:"湯麵（大）",price:65,type:"noodle"},
+    {name:"麻醬麵（小）",price:60,type:"noodle"},
+    {name:"麻醬麵（大）",price:70,type:"noodle"},
+    {name:"鮮蝦餛飩乾麵（小）",price:70,type:"noodle"},
+    {name:"鮮蝦餛飩乾麵（大）",price:80,type:"noodle"},
+    {name:"鮮蝦餛飩湯麵（小）",price:70,type:"noodle"},
+    {name:"鮮蝦餛飩湯麵（大）",price:80,type:"noodle"}
   ],
 
   "關東煮":[
-    {name:"黑輪",price:10},
-    {name:"米血",price:10},
-    {name:"魚板",price:10},
-    {name:"蛋丸",price:10},
-    {name:"菜頭",price:15},
-    {name:"竹輪",price:15},
-    {name:"貢丸",price:20},
-    {name:"福州丸",price:20},
-    {name:"黃金魚板",price:20},
-    {name:"高麗菜捲",price:20},
-    {name:"苦瓜封",price:40}
+    {name:"黑輪",price:10,type:"oden"},
+    {name:"米血",price:10,type:"oden"},
+    {name:"魚板",price:10,type:"oden"},
+    {name:"蛋丸",price:10,type:"oden"},
+    {name:"菜頭",price:15,type:"oden"},
+    {name:"竹輪",price:15,type:"oden"},
+    {name:"貢丸",price:20,type:"oden"},
+    {name:"福州丸",price:20,type:"oden"},
+    {name:"黃金魚板",price:20,type:"oden"},
+    {name:"高麗菜捲",price:20,type:"oden"},
+    {name:"苦瓜封",price:40,type:"oden"}
   ],
 
   "小菜":[
