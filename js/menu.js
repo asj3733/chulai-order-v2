d1482b0f1b3246c356239b7eb6aba60a94d1ab36
asj3733 <asj3733@yahoo.com.tw>
2026-07-20 12:51:22 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,32 +1,34 @@
 const menu = {
 
   "麵類":[
-    {name:"肉燥乾麵（小）",price:55,type:"noodle"},
-    {name:"肉燥乾麵（大）",price:65,type:"noodle"},
-    {name:"肉燥米粉（小）",price:55,type:"noodle"},
-    {name:"肉燥米粉（大）",price:65,type:"noodle"},
-    {name:"湯麵（小）",price:55,type:"noodle"},
-    {name:"湯麵（大）",price:65,type:"noodle"},
-    {name:"麻醬麵（小）",price:60,type:"noodle"},
-    {name:"麻醬麵（大）",price:70,type:"noodle"},
-    {name:"鮮蝦餛飩乾麵（小）",price:70,type:"noodle"},
-    {name:"鮮蝦餛飩乾麵（大）",price:80,type:"noodle"},
-    {name:"鮮蝦餛飩湯麵（小）",price:70,type:"noodle"},
-    {name:"鮮蝦餛飩湯麵（大）",price:80,type:"noodle"}
+    {id:"N001",name:"肉燥乾麵（小）",price:55,type:"noodle"},
+    {id:"N002",name:"肉燥乾麵（大）",price:65,type:"noodle"},
+    {id:"R001",name:"肉燥米粉（小）",price:55,type:"riceNoodle"},
+    {id:"R002",name:"肉燥米粉（大）",price:65,type:"riceNoodle"},
+    {id:"N003",name:"湯麵（小）",price:55,type:"noodle"},
+    {id:"N004",name:"湯麵（大）",price:65,type:"noodle"},
+    {id:"R003",name:"米粉湯（小）",price:55,type:"riceNoodle"},
+    {id:"R004",name:"米粉湯（大）",price:65,type:"riceNoodle"},
+    {id:"N005",name:"麻醬麵（小）",price:60,type:"noodle"},
+    {id:"N006",name:"麻醬麵（大）",price:70,type:"noodle"},
+    {id:"N007",name:"鮮蝦餛飩乾麵（小）",price:70,type:"noodle"},
+    {id:"N008",name:"鮮蝦餛飩乾麵（大）",price:80,type:"noodle"},
+    {id:"N009",name:"鮮蝦餛飩湯麵（小）",price:70,type:"noodle"},
+    {id:"N0010",name:"鮮蝦餛飩湯麵（大）",price:80,type:"noodle"}
   ],
 
   "關東煮":[
-    {name:"黑輪",price:10,type:"oden"},
-    {name:"米血",price:10,type:"oden"},
-    {name:"魚板",price:10,type:"oden"},
-    {name:"蛋丸",price:10,type:"oden"},
-    {name:"菜頭",price:15,type:"oden"},
-    {name:"竹輪",price:15,type:"oden"},
-    {name:"貢丸",price:20,type:"oden"},
-    {name:"福州丸",price:20,type:"oden"},
-    {name:"黃金魚板",price:20,type:"oden"},
-    {name:"高麗菜捲",price:20,type:"oden"},
-    {name:"苦瓜封",price:40,type:"oden"}
+    {id:"O001",name:"黑輪",price:10,type:"oden"},
+    {id:"O002",name:"米血",price:10,type:"oden"},
+    {id:"O003",name:"魚板",price:10,type:"oden"},
+    {id:"O004",name:"蛋丸",price:10,type:"oden"},
+    {id:"O005",name:"菜頭",price:15,type:"oden"},
+    {id:"O006",name:"竹輪",price:15,type:"oden"},
+    {id:"O007",name:"貢丸",price:20,type:"oden"},
+    {id:"O008",name:"福州丸",price:20,type:"oden"},
+    {id:"O009",name:"黃金魚板",price:20,type:"oden"},
+    {id:"O010",name:"高麗菜捲",price:20,type:"oden"},
+    {id:"O011",name:"苦瓜封",price:40,type:"oden"}
   ],
 
   "小菜":[
