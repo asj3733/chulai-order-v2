81cd1b6c60a9149fa55e5d2140ce1ce5d966507f
asj3733 <asj3733@yahoo.com.tw>
2026-07-21 23:00:06 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,57 +1,311 @@
+/* =========================================
+   初萊食麵
+   商品資料
+========================================= */
+
 const menu = {
 
-  "麵類":[
-    {id:"N001",name:"肉燥乾麵（小）",price:55,type:"noodle"},
-    {id:"N002",name:"肉燥乾麵（大）",price:65,type:"noodle"},
-    {id:"R001",name:"肉燥米粉（小）",price:55,type:"riceNoodle"},
-    {id:"R002",name:"肉燥米粉（大）",price:65,type:"riceNoodle"},
-    {id:"N003",name:"湯麵（小）",price:55,type:"noodle"},
-    {id:"N004",name:"湯麵（大）",price:65,type:"noodle"},
-    {id:"R003",name:"米粉湯（小）",price:55,type:"riceNoodle"},
-    {id:"R004",name:"米粉湯（大）",price:65,type:"riceNoodle"},
-    {id:"N005",name:"麻醬麵（小）",price:60,type:"noodle"},
-    {id:"N006",name:"麻醬麵（大）",price:70,type:"noodle"},
-    {id:"N007",name:"鮮蝦餛飩乾麵（小）",price:70,type:"noodle"},
-    {id:"N008",name:"鮮蝦餛飩乾麵（大）",price:80,type:"noodle"},
-    {id:"N009",name:"鮮蝦餛飩湯麵（小）",price:70,type:"noodle"},
-    {id:"N0010",name:"鮮蝦餛飩湯麵（大）",price:80,type:"noodle"}
-  ],
+    /* =========================
+       麵類
+    ========================= */
 
-  "關東煮":[
-    {id:"O001",name:"黑輪",price:10,type:"oden"},
-    {id:"O002",name:"米血",price:10,type:"oden"},
-    {id:"O003",name:"魚板",price:10,type:"oden"},
-    {id:"O004",name:"蛋丸",price:10,type:"oden"},
-    {id:"O005",name:"菜頭",price:15,type:"oden"},
-    {id:"O006",name:"竹輪",price:15,type:"oden"},
-    {id:"O007",name:"貢丸",price:20,type:"oden"},
-    {id:"O008",name:"福州丸",price:20,type:"oden"},
-    {id:"O009",name:"黃金魚板",price:20,type:"oden"},
-    {id:"O010",name:"高麗菜捲",price:20,type:"oden"},
-    {id:"O011",name:"苦瓜封",price:40,type:"oden"}
-  ],
+    "麵類": [
 
-  "小菜":[
-    {name:"麵輪",price:10},
-    {name:"滷蛋",price:15},
-    {name:"荷包蛋",price:15},
-    {name:"蔥蛋",price:40},
-    {name:"香腸",price:25},
-    {name:"手工大腸",price:75}
-  ],
+        {
+            id: "N001",
+            name: "肉燥乾麵（小）",
+            price: 55,
+            type: "noodle"
+        },
 
-  "湯類":[
-    {name:"菜頭湯",price:35},
-    {name:"魚丸湯",price:35},
-    {name:"貢丸湯",price:40},
-    {name:"鮮蝦餛飩湯",price:45}
-  ],
+        {
+            id: "N002",
+            name: "肉燥乾麵（大）",
+            price: 65,
+            type: "noodle"
+        },
 
-  "其他":[
-    {name:"肉燥飯",price:35},
-    {name:"白飯",price:15},
-    {name:"燙青菜",price:40},
-    {name:"水餃",price:50}
-  ]
+        {
+            id: "R001",
+            name: "肉燥米粉（小）",
+            price: 55,
+            type: "riceNoodle"
+        },
+
+        {
+            id: "R002",
+            name: "肉燥米粉（大）",
+            price: 65,
+            type: "riceNoodle"
+        },
+
+        {
+            id: "N003",
+            name: "湯麵（小）",
+            price: 55,
+            type: "noodle"
+        },
+
+        {
+            id: "N004",
+            name: "湯麵（大）",
+            price: 65,
+            type: "noodle"
+        },
+
+        {
+            id: "R003",
+            name: "米粉湯（小）",
+            price: 55,
+            type: "riceNoodle"
+        },
+
+        {
+            id: "R004",
+            name: "米粉湯（大）",
+            price: 65,
+            type: "riceNoodle"
+        },
+
+        {
+            id: "N005",
+            name: "麻醬麵（小）",
+            price: 60,
+            type: "noodle"
+        },
+
+        {
+            id: "N006",
+            name: "麻醬麵（大）",
+            price: 70,
+            type: "noodle"
+        },
+
+        {
+            id: "N007",
+            name: "鮮蝦餛飩乾麵（小）",
+            price: 70,
+            type: "noodle"
+        },
+
+        {
+            id: "N008",
+            name: "鮮蝦餛飩乾麵（大）",
+            price: 80,
+            type: "noodle"
+        },
+
+        {
+            id: "N009",
+            name: "鮮蝦餛飩湯麵（小）",
+            price: 70,
+            type: "noodle"
+        },
+
+        {
+            id: "N010",
+            name: "鮮蝦餛飩湯麵（大）",
+            price: 80,
+            type: "noodle"
+        }
+
+    ],
+
+
+    /* =========================
+       關東煮
+    ========================= */
+
+    "關東煮": [
+
+        {
+            id: "O001",
+            name: "黑輪",
+            price: 10,
+            type: "oden"
+        },
+
+        {
+            id: "O002",
+            name: "米血",
+            price: 10,
+            type: "oden"
+        },
+
+        {
+            id: "O003",
+            name: "魚板",
+            price: 10,
+            type: "oden"
+        },
+
+        {
+            id: "O004",
+            name: "蛋丸",
+            price: 10,
+            type: "oden"
+        },
+
+        {
+            id: "O005",
+            name: "菜頭",
+            price: 15,
+            type: "oden"
+        },
+
+        {
+            id: "O006",
+            name: "竹輪",
+            price: 15,
+            type: "oden"
+        },
+
+        {
+            id: "O007",
+            name: "貢丸",
+            price: 20,
+            type: "oden"
+        },
+
+        {
+            id: "O008",
+            name: "福州丸",
+            price: 20,
+            type: "oden"
+        },
+
+        {
+            id: "O009",
+            name: "黃金魚板",
+            price: 20,
+            type: "oden"
+        },
+
+        {
+            id: "O010",
+            name: "高麗菜捲",
+            price: 20,
+            type: "oden"
+        },
+
+        {
+            id: "O011",
+            name: "苦瓜封",
+            price: 40,
+            type: "oden"
+        }
+
+    ],
+
+
+    /* =========================
+       小菜
+    ========================= */
+
+    "小菜": [
+
+        {
+            id: "S001",
+            name: "麵輪",
+            price: 10
+        },
+
+        {
+            id: "S002",
+            name: "滷蛋",
+            price: 15
+        },
+
+        {
+            id: "S003",
+            name: "荷包蛋",
+            price: 15
+        },
+
+        {
+            id: "S004",
+            name: "蔥蛋",
+            price: 40
+        },
+
+        {
+            id: "S005",
+            name: "香腸",
+            price: 25
+        },
+
+        {
+            id: "S006",
+            name: "手工大腸",
+            price: 75
+        }
+
+    ],
+
+
+    /* =========================
+       湯類
+    ========================= */
+
+    "湯類": [
+
+        {
+            id: "T001",
+            name: "菜頭湯",
+            price: 35
+        },
+
+        {
+            id: "T002",
+            name: "魚丸湯",
+            price: 35
+        },
+
+        {
+            id: "T003",
+            name: "貢丸湯",
+            price: 40
+        },
+
+        {
+            id: "T004",
+            name: "鮮蝦餛飩湯",
+            price: 45
+        }
+
+    ],
+
+
+    /* =========================
+       其他
+    ========================= */
+
+    "其他": [
+
+        {
+            id: "O100",
+            name: "肉燥飯",
+            price: 35
+        },
+
+        {
+            id: "O101",
+            name: "白飯",
+            price: 15
+        },
+
+        {
+            id: "O102",
+            name: "燙青菜",
+            price: 40
+        },
+
+        {
+            id: "O103",
+            name: "水餃",
+            price: 50
+        }
+
+    ]
 
 };
